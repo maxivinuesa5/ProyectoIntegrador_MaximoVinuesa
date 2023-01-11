fa10f9253be0e4f1568a34a819f161b6d2f61d28
maxivinuesa5 <maxivinuesa5@gmail.com>
2023-01-11 00:47:22 -0300

    BackEnd
    Creacion backend, mas enlace de workbench, sql

---
+++ BackEnd/src/main/java/com/portfolioargprograma/maxivinuesa2/Maxivinuesa2Application.java
@@ -0,0 +1,13 @@
+package com.portfolioargprograma.maxivinuesa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maxivinuesa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Maxivinuesa2Application.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolioargprograma/maxivinuesa2/Maxivinuesa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioargprograma.maxivinuesa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maxivinuesa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
